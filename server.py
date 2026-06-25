6db2e6a09f83cff43e153569ec6427b888894cc3
David Alejandro Pedroza De Jesús <dapedeje@alumni.uv.es>
2026-06-25 18:42:27 +0100

    Update server.py

--- server.py
+++ server.py
@@ -7,21 +7,20 @@ from pycoral.utils.edgetpu import make_interpreter
 from pycoral.adapters import classify
 from pycoral.adapters.common import input_size
 
-# CONFIG
+
 MODEL_PATH = "ResNet50_edgetpu.tflite"
 LABELS_PATH = "labels.txt"
 
-# cargar etiquetas
+
 labels = {}
 with open(LABELS_PATH, "r") as f:
     for i, line in enumerate(f):
         labels[i] = line.strip()
 
-# cargar modelo
+
 interpreter = make_interpreter(MODEL_PATH)
 interpreter.allocate_tensors()
 
-# cámara
 cap = cv2.VideoCapture("/dev/video1")
 
 app = Flask(__name__)
@@ -32,12 +31,12 @@ def predict():
     if not ret:
         return jsonify({"error": "no frame"})
 
-    # preprocess
+
     size = input_size(interpreter)
     frame_resized = cv2.resize(frame, size)
     frame_rgb = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
 
-    # inferencia
+
     from pycoral.adapters.common import set_input
     set_input(interpreter, frame_rgb)
     interpreter.invoke()
